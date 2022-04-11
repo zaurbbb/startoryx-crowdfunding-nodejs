4131ec5cf906549b3c4557b3942585450532d56d
Bioneisme <bioneisme@gmail.com>
2022-04-11 20:50:30 +0600

    init config

---
+++ server/config/passport.js
@@ -0,0 +1,38 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const User = require('../models/user-model')
+const userService = require('../services/user-service')
+
+module.exports = function (passport, req, res){
+    passport.serializeUser((user, done) =>{
+        done(null, user.id)
+    })
+    passport.deserializeUser((id, done) =>{
+        User.findById(id, function (err, user){
+            done(err, user)
+        })
+    })
+
+    passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://localhost:5000/api/google/callback"
+    },
+        // TODO: Rewrite to user-controller
+        async(accessToken, refreshToken, profile, done)=>{
+            User.findOne({googleId: profile.id}).then((currentUser) => {
+                if (currentUser){
+                    console.log(currentUser)
+                    console.log(profile)
+                    done(null, currentUser)
+                }
+                else {
+                    userService.registrationByGoogle(profile.id, profile.name.givenName,
+                        profile.name.familyName, accessToken)
+                        .then((newUser)=>{
+                        done(null, newUser)
+                    });
+                }
+            })
+        })
+    )
+}
