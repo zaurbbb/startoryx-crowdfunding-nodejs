0971b649cb8aa67ee14c512e0f4c5a22667435e7
Bioneisme <bioneisme@gmail.com>
2022-04-11 23:42:40 +0600

    updated user-model.js

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,8 +1,9 @@
 const {Schema, model} = require('mongoose')
 
 const userSchema = new Schema({ // TODO: add required fields
-    email: {type: String, unique: true, required: true},
-    password: {type: String, required: true},
+    email: {type: String},
+    password: {type: String},
+    googleId: {type: String},
     first_name: {type: String},
     last_name: {type: String},
     phone: {type: String},
