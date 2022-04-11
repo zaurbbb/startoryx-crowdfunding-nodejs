e7e9ad784f932f6436012adb3dd1afda72278f06
Bioneisme <bioneisme@gmail.com>
2022-04-11 23:46:13 +0600

    updated token-service.js

--- server/services/token-service.js
+++ server/services/token-service.js
@@ -3,8 +3,7 @@ const tokenModel = require('../models/token-model')
 
 class TokenService{
     // create access and refresh tokens
-    generateTokens(payload){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '1h'})
+    generateTokens(payload, accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn: '1h'})){
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn: '30d'})
         return {
             accessToken,
