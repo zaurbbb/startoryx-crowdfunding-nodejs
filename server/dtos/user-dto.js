658a6b322f1254c571d11fc6c89704c56707e60a
Bioneisme <bioneisme@gmail.com>
2022-04-11 20:52:21 +0600

    cut down user dto

--- server/dtos/user-dto.js
+++ server/dtos/user-dto.js
@@ -2,10 +2,6 @@
 module.exports = class UserDto{
     id;
     email;
-    first_name;
-    last_name;
-    phone;
-    age;
     isActivated;
     roles;
 
@@ -14,9 +10,5 @@ module.exports = class UserDto{
         this.id = model._id;
         this.isActivated = model.isActivated;
         this.roles = model.roles;
-        this.first_name = model.first_name;
-        this.last_name = model.last_name;
-        this.phone = model.phone;
-        this.age = model.age;
     }
 }
