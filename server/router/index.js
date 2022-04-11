c0125e66a595d92dfa9473ce5dfdc24162da6d45
Bioneisme <bioneisme@gmail.com>
2022-04-11 23:44:36 +0600

    added oauth2 routers

--- server/router/index.js
+++ server/router/index.js
@@ -1,8 +1,13 @@
 const Router = require('express').Router
 const userController = require('../controllers/user-controller')
 const router = new Router()
+const passport = require('passport')
 const {body} = require('express-validator')
 const roleMiddleware = require('../middlewares/role-middleware')
+
+function isLoggedIn(req, res, next){
+    req.user ? next() : res.sendStatus(401)
+}
 
 router.post('/registration',
     body('email').isEmail(),
@@ -11,6 +16,26 @@ router.post('/registration',
 )
 router.post('/login', userController.login)
 router.post('/logout', userController.logout)
+
+
+router.get('/googleLog', (req, res) =>{
+    res.render('login')
+})
+router.get('/dashboard', (req, res) =>{
+    res.render('dashboard')
+})
+router.get('/protected', isLoggedIn, (req, res) =>{
+    res.render('protected')
+})
+
+router.get('/google', passport.authenticate('google', { scope: ['email', 'profile'] }))
+
+router.get('/google/callback', passport.authenticate('google', {
+    successRedirect: "/api/protected",
+    failureRedirect: "/"}),
+    (req, res) => {
+        res.redirect('/api/dashboard')
+    });
 
 router.get('/activate/:link', userController.activate)
 router.get('/refresh', userController.refresh)
