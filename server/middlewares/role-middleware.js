b96512ae88eabcd1ae2b623702d9618536eb43a0
Bioneisme <bioneisme@gmail.com>
2022-04-13 01:30:13 +0600

    remade role MW for the passport auth

--- server/middlewares/role-middleware.js
+++ server/middlewares/role-middleware.js
@@ -1,18 +1,10 @@
 const ApiError = require('../exceptions/api-error')
-const tokenService = require('../services/token-service')
 
 module.exports = function (roles) {
     return function (req, res, next) {
-        if (req.method === "OPTIONS") {
-            next()
-        }
-
         try {
-            const token = req.headers.authorization.split(' ')[1]
-            if (!token) {
-                return next(ApiError.NoAccess())
-            }
-            const {roles: userRoles} = tokenService.validateAccessToken(token)
+            const userRoles = req.user.roles;
+
             let hasRole = false
             userRoles.forEach(role => {
                 if (roles.includes(role)) {
