e4f826c2afe6471c0be2f4e568ff334cf1a468cd
Bioneisme <bioneisme@gmail.com>
2022-04-14 22:56:18 +0600

    init admin panel

---
+++ server/admin/resources/panel-locale.js
@@ -0,0 +1,12 @@
+module.exports = {
+    locale: {
+        translations: {
+            labels: {
+                loginWelcome: 'Login'
+            },
+            messages: {
+                loginWelcome: 'Authorization page of the admin panel for managing the Startoryx site'
+            }
+        }
+    }
+}
---
+++ server/admin/resources/user-resource.js
@@ -0,0 +1,85 @@
+module.exports = {
+    properties: {
+        email: {
+            type: 'email',
+            isVisible: {
+                show: true,
+                edit: false,
+                list: true,
+                filter: true
+            }
+        },
+        password: {
+            isVisible: {
+                show: true,
+                edit: false,
+                list: false,
+                filter: false
+            }
+        },
+        _id: {
+            isVisible: {
+                show: true,
+                edit: false,
+                list: false,
+                filter: false
+            }
+        },
+        googleId: {
+            isVisible: {
+                show: true,
+                edit: false,
+                list: true,
+                filter: false
+            }
+        },
+        first_name: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        last_name: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        phone: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        age: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        isActivated: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        roles: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+    }
+}
