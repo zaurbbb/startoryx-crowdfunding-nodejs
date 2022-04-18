34e92c960243cb4853b8e42dff55880cef8db6b9
Bioneisme <bioneisme@gmail.com>
2022-04-18 08:30:43 +0600

    init project-resource.js

---
+++ server/admin/resources/project-resource.js
@@ -0,0 +1,92 @@
+module.exports = {
+    properties: {
+        title: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        type: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        _id: {
+            isVisible: {
+                show: true,
+                edit: false,
+                list: false,
+                filter: false
+            }
+        },
+        shortly: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: false
+            }
+        },
+        body: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: false,
+                filter: false
+            }
+        },
+        user: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        dateCreated: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        days: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        goal: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        collected: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+        rate: {
+            isVisible: {
+                show: true,
+                edit: true,
+                list: true,
+                filter: true
+            }
+        },
+    }
+}
