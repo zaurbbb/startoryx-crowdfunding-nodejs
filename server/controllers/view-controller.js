e4b9bc39ac7391580a50ddc2a3d704c2cf3fb22d
Bioneisme <bioneisme@gmail.com>
2022-04-30 00:16:13 +0600

    init view controller

---
+++ server/controllers/view-controller.js
@@ -0,0 +1,29 @@
+const Project = require("../models/project-model");
+const projectController = require("./project-controller");
+const formatDate = require("../helpers/formatDate");
+
+
+class ViewController{
+    async dashboard(req, res, next){
+        try{
+            let search = null
+            let projects
+            if (req._parsedUrl.query != null) {
+                const str = req._parsedUrl.query
+                search = str.split('=',2)[1]
+                projects = await Project.find({$text: {$search: search}}).lean()
+            } else {
+                projects = await Project.find().lean()
+            }
+            const sortedProjects = await projectController.ProjectSort(projects, parseInt(req.params.sort) || 0)
+            let email = null
+            if (req.user != null) email = req.user.email
+            res.render('pages/dashboard.ejs', {email: email, date: formatDate, projects: sortedProjects, search: req._parsedUrl.query})
+        }
+        catch (e) {
+            next(e)
+        }
+    }
+}
+
+module.exports = new ViewController()
