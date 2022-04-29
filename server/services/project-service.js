99de3275e8837c8f8e6e1fec44c531b1b423a027
Bioneisme <bioneisme@gmail.com>
2022-04-30 00:24:45 +0600

    fixed and added average rate sort

--- server/services/project-service.js
+++ server/services/project-service.js
@@ -15,15 +15,25 @@ class ProjectService{
     }
     async ascendingByDate(projects){
         return await projects.sort(function(obj1, obj2) {
-            if (obj1.dateCreated < obj2.dateCreated) return -1;
-            if (obj1.dateCreated > obj2.dateCreated) return 1;
+            if (obj1.dateCreated > obj2.dateCreated) return -1;
+            if (obj1.dateCreated < obj2.dateCreated) return 1;
             return 0;
         })
     }
     async decreasingByDate(projects){
         return await projects.sort(function(obj1, obj2) {
-            if (obj1.dateCreated > obj2.dateCreated) return -1;
-            if (obj1.dateCreated < obj2.dateCreated) return 1;
+            if (obj1.dateCreated < obj2.dateCreated) return -1;
+            if (obj1.dateCreated > obj2.dateCreated) return 1;
+            return 0;
+        })
+    }
+    async ascendingByRate(projects){
+        return await projects.sort(function(obj1, obj2) {
+            const avrRate1 = obj1.rate.reduce((a, b) => parseInt(a) + parseInt(b), 0) / obj1.rate.length
+            const avrRate2 = obj2.rate.reduce((a, b) => parseInt(a) + parseInt(b), 0) / obj2.rate.length
+
+            if (avrRate1 > avrRate2) return -1;
+            if (avrRate1 < avrRate2) return 1;
             return 0;
         })
     }
