e0746e01fd9836d477d9aac67129ec2f140d7230
Bioneisme <bioneisme@gmail.com>
2022-05-05 16:43:13 +0600

    changed default model image

--- server/models/project-model.js
+++ server/models/project-model.js
@@ -4,18 +4,16 @@ const htmlPurify = domPurify(new JSDOM().window)
 
 const {Schema, model} = require('mongoose')
 
-
-
 const projectSchema = new Schema({ // TODO: add required fields
     title: {type: String, required: true},
-    image: {type: String, default: "https://images.thdstatic.com/productImages/27453014-e0f3-406c-98f4-2c217ef3bda8/svn/nuwallpaper-wallpaper-samples-pls4208sam-64_400.jpg"},
-    type: {type: String},
-    shortly: {type: String}, // short description
-    body: {type: String}, // detailed description
+    image: {type: String, default: "https://res.cloudinary.com/dluwizg51/image/upload/v1651747098/PROJECTS/no_image_pp3wpw.png"},
+    type: {type: String, required: true},
+    shortly: {type: String, required: true}, // short description
+    body: {type: String, required: true}, // detailed description
     user: {type: Schema.Types.ObjectId, ref: 'User'}, // creator
     dateCreated: {type: Date, default: Date.now},
-    days: {type: Number}, // number of days before the end of the collection
-    goal: {type: Number},
+    days: {type: Number, required: true}, // number of days before the end of the collection
+    goal: {type: Number, default: 100},
     collected: {type: Number, default: 0},
     rates: [{type: Schema.Types.ObjectId, ref: 'Rate'}],
     avgRate: {type: Number, default: 0},
