9ce24e0a9bb417be25aae9bd066c3ef730ee3ee8
Bioneisme <bioneisme@gmail.com>
2022-05-05 16:20:55 +0600

    moved from project router to view controller

--- server/controllers/view-controller.js
+++ server/controllers/view-controller.js
@@ -1,6 +1,9 @@
 const Project = require("../models/project-model");
+const Comment = require("../models/comment-model");
+const Rate = require("../models/rate-model");
 const projectController = require("./project-controller");
 const formatDate = require("../helpers/formatDate");
+const averageRate = require("../helpers/averageRate");
 
 
 class ViewController{
@@ -21,6 +24,95 @@ class ViewController{
             if (req._parsedUrl.query != null) searchUrl = req._parsedUrl.query
             if (req.user != null) email = req.user.email
             res.render('pages/dashboard.ejs', {email: email, date: formatDate, projects: sortedProjects, search: searchUrl})
+        }
+        catch (e) {
+            next(e)
+        }
+    }
+    async getProject(req, res, next){
+        try {
+            let email = null
+            if (req.user != null) email = req.user.email
+            await Project.findById(req.params.id).populate({path: 'comments', model: 'Comment',
+                populate: {path: 'user', model: 'User'}}).populate('user').
+            exec(function(err, project) {
+                if (err) {
+                    console.log(err)
+                }
+                res.render('projects/read.ejs', {email: email, date: formatDate, project: project})
+            })
+        }
+        catch (e) {
+            next(e)
+        }
+    }
+    async putProject(req, res, next){
+        try {
+            await Project.findOneAndUpdate(
+                {_id: req.params.id},
+                {
+                    title: req.body.title, body: req.body.body, shortly: req.body.shortly, days: req.body.days,
+                    type: req.body.type, goal: req.body.goal
+                })
+            res.redirect('/api/dashboard')
+        } catch (e) {
+            next(e)
+        }
+    }
+    async getEditProject(req, res, next){
+        try {
+            const project = await Project.findById(req.params.id).lean()
+            if (project.user.equals(req.user._id))
+                res.render('projects/edit.ejs', {email: req.user.email, project: project})
+            else
+                res.redirect('/api/dashboard')
+        } catch (e) {
+            next(e)
+        }
+    }
+    async postProject(req, res, next){
+        try {
+            req.body.user = req.user._id
+            req.body.image = req.file.path
+            const project = await Project.create(req.body)
+            console.log(project)
+            res.redirect('/api/dashboard')
+        } catch (e) {
+            next(e)
+        }
+    }
+    async postComment(req, res, next){
+        try {
+            req.body.project = req.params.id
+            req.body.user = req.user._id
+            const comment = await Comment.create(req.body)
+            await Project.findOneAndUpdate(
+                {_id: req.params.id},
+                {$push: {comments: comment}})
+
+            console.log(comment)
+            res.redirect('back')
+        }
+        catch (e) {
+            next(e)
+        }
+    }
+    async postRate(req, res, next){
+        try {
+            req.body.rate = parseInt(req.body.rate)
+            req.body.project = req.params.id
+            req.body.user = req.user._id
+            const rate = await Rate.create(req.body)
+            await Project.findOneAndUpdate(
+                {_id: req.params.id},
+                {$push: {rates: rate}})
+
+            await Project.findOneAndUpdate({
+                _id: req.params.id}, {
+                avgRate: await averageRate(req.params.id)
+            })
+            console.log(rate)
+            res.redirect('back')
         }
         catch (e) {
             next(e)
--- server/router/project-router.js
+++ server/router/project-router.js
@@ -1,15 +1,10 @@
 const Router = require('express').Router
-const Project = require('../models/project-model')
-const Comment = require('../models/comment-model')
-const Rate = require('../models/rate-model')
-const {ensureAuth} = require('../middlewares/auth-middleware')
+const {ensureAuth, alreadyRated} = require('../middlewares/auth-middleware')
 const router = new Router()
-const formatDate = require('../helpers/formatDate')
-const averageRate = require('../helpers/averageRate')
 const multer = require('multer')
 const cloudinary = require('cloudinary').v2
 const {CloudinaryStorage} = require("multer-storage-cloudinary")
-const authMiddleware = require("../middlewares/auth-middleware")
+const viewController = require('../controllers/view-controller')
 
 
 cloudinary.config({
@@ -27,104 +22,22 @@ const storage = new CloudinaryStorage({
 
 const upload = multer({storage: storage});
 
+
 router.get('/add', ensureAuth, (req, res) => {
     res.render('projects/add.ejs', {email: req.user.email})
 })
 
-router.post('/add', ensureAuth, upload.single('image', {width: 305, height: 305, crop: "fill"}), async (req, res) => {
-    try {
-        req.body.user = req.user._id
-        req.body.image = req.file.path
-        const project = await Project.create(req.body)
-        console.log(project)
-        res.redirect('/api/dashboard')
-    } catch (e) {
-        console.log(e)
-    }
-})
+router.post('/add', ensureAuth, upload.single('image', {width: 305, height: 305, crop: "fill"}), viewController.postProject)
 
-router.get('/edit/:id', async (req, res) => {
-    try {
-        const project = await Project.findById(req.params.id).lean()
-        if (project.user.equals(req.user._id))
-            res.render('projects/edit.ejs', {email: req.user.email, project: project})
-        else
-            res.redirect('/api/dashboard')
-    } catch (e) {
-        console.log(e)
-    }
-})
+router.get('/edit/:id', viewController.getEditProject)
 
-router.put('/:id', async (req, res) => {
-    try {
-        await Project.findOneAndUpdate(
-            {_id: req.params.id},
-            {
-                title: req.body.title, body: req.body.body, shortly: req.body.shortly, days: req.body.days,
-                type: req.body.type, goal: req.body.goal
-            })
-        res.redirect('/api/dashboard')
-    } catch (e) {
-        console.log(e)
-    }
-})
+router.put('/:id', viewController.putProject)
 
-router.get('/:id', async (req, res) => {
-    try {
-        let email = null
-        if (req.user != null) email = req.user.email
-        await Project.findById(req.params.id).populate({path: 'comments', model: 'Comment',
-        populate: {path: 'user', model: 'User'}}).populate('user').
-        exec(function(err, project) {
-            if (err) {
-                console.log(err)
-            }
-            res.render('projects/read.ejs', {email: email, date: formatDate, project: project})
-        })
+router.get('/:id', viewController.getProject)
 
-    } catch (e) {
-        console.log(e)
-    }
-})
+router.post('/:id/comment', ensureAuth, viewController.postComment)
 
-router.post('/:id/comment', authMiddleware.ensureAuth, async (req, res) => {
-    try {
-        req.body.project = req.params.id
-        req.body.user = req.user._id
-        const comment = await Comment.create(req.body)
-        await Project.findOneAndUpdate(
-            {_id: req.params.id},
-            {$push: {comments: comment}})
-
-        console.log(comment)
-        res.redirect('back')
-    }
-    catch (e) {
-        console.log(e)
-    }
-})
-
-router.post('/:id/rate', authMiddleware.ensureAuth, authMiddleware.alreadyRated, async (req, res) => {
-    try {
-        req.body.rate = parseInt(req.body.rate)
-        req.body.project = req.params.id
-        req.body.user = req.user._id
-        const rate = await Rate.create(req.body)
-        await Project.findOneAndUpdate(
-            {_id: req.params.id},
-            {$push: {rates: rate}})
-
-        await Project.findOneAndUpdate({
-            _id: req.params.id}, {
-            avgRate: await averageRate(req.params.id)
-        })
-        console.log(rate)
-        res.redirect('back')
-    }
-    catch (e) {
-        console.log(e)
-    }
-})
+router.post('/:id/rate', ensureAuth, alreadyRated, viewController.postRate)
 
 
 // router.delete('/delete/:id', async (req, res) => {
