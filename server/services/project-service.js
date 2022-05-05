2197c873b21b898a329e1bfb39bd9d0ce14bc190
Bioneisme <bioneisme@gmail.com>
2022-05-05 15:22:43 +0600

    changed avg rate sort

--- server/services/project-service.js
+++ server/services/project-service.js
@@ -29,11 +29,8 @@ class ProjectService{
     }
     async ascendingByRate(projects){
         return await projects.sort(function(obj1, obj2) {
-            const avrRate1 = obj1.rate.reduce((a, b) => parseInt(a) + parseInt(b), 0) / obj1.rate.length
-            const avrRate2 = obj2.rate.reduce((a, b) => parseInt(a) + parseInt(b), 0) / obj2.rate.length
-
-            if (avrRate1 > avrRate2) return -1;
-            if (avrRate1 < avrRate2) return 1;
+            if (obj1.avgRate > obj2.avgRate) return -1;
+            if (obj1.avgRate < obj2.avgRate) return 1;
             return 0;
         })
     }
