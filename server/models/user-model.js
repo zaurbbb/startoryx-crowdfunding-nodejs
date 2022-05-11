81ce083185cf21a1f9c6653d1d664e34ea31ec2e
Bioneisme <bioneisme@gmail.com>
2022-05-11 12:35:04 +0600

    nickname, default no ava image

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -2,9 +2,10 @@ const {Schema, model} = require('mongoose')
 
 const userSchema = new Schema({
     email: {type: String, unique: true},
+    nickname: {type: String, unique: true},
     password: {type: String},
     googleId: {type: String},
-    image: {type: String},
+    image: {type: String, default: "https://res.cloudinary.com/dluwizg51/image/upload/v1652250733/AVATARS/no-pic-ava_golenz.jpg"},
     first_name: {type: String},
     last_name: {type: String},
     phone: {type: String},
