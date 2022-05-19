fbdbf039b7c560305fb24d6b2eb5d82682ae5891
Bioneisme <bioneisme@gmail.com>
2022-05-19 23:37:51 +0600

    dynamic url navigation from env

--- controllers/passport.js
+++ controllers/passport.js
@@ -30,7 +30,7 @@ module.exports = function (passport, req, res) {
     passport.use(new GoogleStrategy({
                 clientID: process.env.GOOGLE_CLIENT_ID,
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-                callbackURL: process.env.CLIENT_UNT + "/api/google/callback"
+                callbackURL: process.env.CLIENT_URL + "/api/google/callback"
             },
             async (accessToken, refreshToken, profile, done) => {
                 try {
